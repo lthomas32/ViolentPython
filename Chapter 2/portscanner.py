d064b125e8b5ed5487e3e5f467172454af77539b
lthomas32 <lthomas32@ewu.edu>
2023-11-22 17:24:28 -0800

    Updated to take list and run main

--- Chapter 2/portscanner.py
+++ Chapter 2/portscanner.py
@@ -10,14 +10,14 @@ parser = optparse.OptionParser('portscanner.py' +
 parser.add_option('-H', dest='tgtHost', type='string',
                   help="Specify target host")
 # Adds the -p to take an int argument
-parser.add_option('-p', dest='tgtPort', type='int',
-                  help="Specify target port")
+parser.add_option('-p', dest='tgtPort', type='str',
+                  help="Specify target ports by , seperated values")
 parser.add_option('-v', dest='bannerInfo', default=False,
                   action="store_true", help="Grab banner information from the port")
 # returns a dictionary like object from optparse.Values into options and a list of leftover arguments
 options, args = parser.parse_args()
 target = options.tgtHost
-port = options.tgtPort
+port = options.tgtPort.split(",")
 bannerInfo = options.bannerInfo
 
 if (target is None) | (port is None):
@@ -25,3 +25,6 @@ if (target is None) | (port is None):
     sys.exit(0)
 def main():
     ScannerMethods.port_scan(target, port, bannerInfo)
+
+if __name__ == "__main__":
+    main()
