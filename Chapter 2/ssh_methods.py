633cab0dd411aa26dd6990f65fed77e6ae2eb86d
lthomas32 <lthomas32@ewu.edu>
2023-12-08 20:52:24 -0800

    added ssh_methods module for testing

--- Chapter 2/ssh_methods.py
+++ Chapter 2/ssh_methods.py
@@ -1,9 +1,12 @@
-import pxssh
+from pexpect import pxssh
+
 
 def send_command(s, cmd):
     s.sendline(cmd)
-    s.prompt()
-    print(s.before)
+    s.sendline("password")
+    if s.prompt():
+        print(s.before)
+
 
 def connect(host, user, password):
     try:
@@ -14,6 +17,11 @@ def connect(host, user, password):
         print("Error connecting")
         exit(0)
 
+
 def main():
     s = connect("192.168.1.30", "lewis", "password")
-    send_command(s, "cat /etc/shadow | grep root")
+    send_command(s, "sudo cat /etc/shadow | grep root")
+
+
+if __name__ == "__main__":
+    main()
